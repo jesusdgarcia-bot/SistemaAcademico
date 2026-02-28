477e0f570a78d5ac9957ebe6b5fe88f63394ad93
Jssdvdgrcprz <jssdvdgrcprz@gmail.com>
2026-02-27 22:28:46 -0500

    feat: inicializar proyecto SistemaAcademico

---
+++ src/sistemaacademico/SistemaAcademico.java
@@ -0,0 +1,16 @@
+package sistemaacademico;
+
+/**
+ *
+ * @author jssdv
+ */
+public class SistemaAcademico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
