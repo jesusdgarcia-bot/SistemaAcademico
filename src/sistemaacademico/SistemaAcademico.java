7d80fa1d855fc7264892df368986ce0057df6275
Jssdvdgrcprz <jssdvdgrcprz@gmail.com>
2026-02-28 11:19:57 -0500

    feat: implementar menu principalcon todas las opciones

--- src/sistemaacademico/SistemaAcademico.java
+++ src/sistemaacademico/SistemaAcademico.java
@@ -11,6 +11,36 @@ public class SistemaAcademico {
 
     
     public static void main(String[] args) {
-       
+      mostrarMenu();
     }
+    
+    public static void mostrarMenu() {
+        Scanner leer = new Scanner(System.in);
+        int opcion = 0;
+
+        do {
+            System.out.println("\n . . . : : :SISTEMA ACADEMICO: : : . . .");
+            System.out.println("1. Registrar estudiante");
+            System.out.println("2. Listar estudiantes");
+            System.out.println("3. Buscar estudiante por numero de identificacion");
+            System.out.println("4. Actualizar estudiante");
+            System.out.println("5. Eliminar estudiante");
+            System.out.println("6. Salir");
+            System.out.print("Seleccione una opcion: ");
+            
+            opcion = leer.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Opcion 1 seleccionada...");
+                    break;
+                case 6:
+                    System.out.println("Saliendo del sistema...");
+                    break;
+                default:
+                    System.out.println("Opcion no valida.");
+            }
+        } while (opcion >= 6 && opcion <= 1);
+    }
+    
 }
