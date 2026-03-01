4609aec75858a75e61051184b2dd45e2060a0e74
Abisael Freyle Almenarez <afreylea@unicesar.edu.co>
2026-03-01 17:22:23 -0500

    feat: crear clase Nota con atributos

---
+++ src/sistemaacademico/Nota.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemaacademico;
+
+/**
+ *
+ * @author abisa
+ */
+public class Nota {
+    private Estudiante estudiante;
+    private Asignatura asignatura;
+    private double valor;
+    private int periodo;
+}
