2dd83000cc0cbc37e405e1aaf2a7724f5a9e6fa1
Abisael Freyle Almenarez <afreylea@unicesar.edu.co>
2026-03-01 17:12:09 -0500

    feat: agregar encapsulamiento a clase Asignatura

--- src/sistemaacademico/Asignatura.java
+++ src/sistemaacademico/Asignatura.java
@@ -14,5 +14,49 @@ public class Asignatura {
     private int creditos;
     private String docente;
   
-    
+    public Asignatura(String codigo, String nombre, int creditos, String docente){
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.creditos = creditos;
+        this.docente = docente;
+    }
+  
+    public Asignatura(){
+      this.codigo = null;
+      this.nombre = null;
+      this.creditos = 0;
+      this.docente = null;
+    }
+  
+    public String getCodigo(){
+      return this.codigo;
+    }
+
+    public String getNombre(){
+      return this.nombre;
+    }
+
+    public int getCreditos(){
+      return this.creditos;
+    }
+
+    public String getDocente(){
+      return this.docente;
+    }
+
+    public void setCodigo(String codigo){
+      this.codigo = codigo;
+    }
+
+    public void setNombre(String nombre){
+      this.nombre = nombre;
+    }
+
+    public void setCreditos(int creditos){
+      this.creditos = creditos;
+    }
+
+    public void setDocente(String docente){
+      this.docente = docente;
+    }
 }
