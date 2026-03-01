914445fc3e75a3c4a4852fdcac88334109bb74e0
Jesus David <jdaviddominguez@unicesar.edu.co>
2026-03-01 14:39:24 -0500

    feat: implementar registrarAsignatura() en Main (CRUD Asignatura parte 1)

---
+++ src/sistemaacademico/Asignatura.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemaacademico;
+
+/**
+ *
+ * @author Equipo
+ */
+class Asignatura {
+
+    Asignatura(String codigo, String nombre, int creditos, String docente) {
+    }
+    
+}
