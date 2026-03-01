939fc5ba379d4e5b15b2f06915aefe372930311f
Abisael Freyle Almenarez <afreylea@unicesar.edu.co>
2026-03-01 17:25:52 -0500

    feat: agregar encapsulamiento a clase Nota

--- src/sistemaacademico/Nota.java
+++ src/sistemaacademico/Nota.java
@@ -13,4 +13,50 @@ public class Nota {
     private Asignatura asignatura;
     private double valor;
     private int periodo;
+      
+    public Nota( Estudiante estudiante, Asignatura asignatura, double valor, int periodo){
+      this.estudiante = estudiante;
+      this.asignatura = asignatura;
+      this.valor = valor;
+      this.periodo = periodo;
+    }
+
+    public Nota(){
+      this.estudiante = null;
+      this.asignatura = null;
+      this.valor = 0.0;
+      this.periodo = 0;
+    }
+
+    public Estudiante getEstudiante(){ 
+      return estudiante; 
+    }
+
+    public Asignatura getAsignatura(){ 
+      return asignatura; 
+    } 
+
+    public double getValor(){ 
+      return valor;
+    } 
+
+    public int getPeriodo(){ 
+      return periodo; 
+    } 
+
+    public void setEstudiante(Estudiante estudiante){ 
+      this.estudiante = estudiante; 
+    }
+
+    public void setAsignatura(Asignatura asignatura){ 
+      this.asignatura = asignatura; 
+    } 
+
+    public void setValor(double valor){ 
+      this.valor = valor; 
+    } 
+
+    public void setPeriodo(int periodo){ 
+      this.periodo = periodo; 
+    } 
 }
